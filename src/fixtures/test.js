f46d0342b8fa01cca346ecfd9eaf92523364690c
maximejoannis <maxime.joannis6@gmail.com>
2026-07-25 13:31:31 +0200

    Add files via upload

--- src/fixtures/test.js
+++ src/fixtures/test.js
@@ -1,1 +1,53 @@
+const base = require('@playwright/test');
 
+const { HomePage } = require('../pages/HomePage');
+const { LoginModal } = require('../pages/LoginModal');
+const {
+  SearchResultsPage,
+} = require('../pages/SearchResultsPage');
+const {
+  ListingPage,
+} = require('../pages/ListingPage');
+const {
+  DashboardPage,
+} = require('../pages/DashboardPage');
+
+
+const test = base.test.extend({
+  homePage: async ({ page }, use) => {
+    const homePage = new HomePage(page);
+
+    await use(homePage);
+  },
+
+  loginModal: async ({ page }, use) => {
+    const loginModal = new LoginModal(page);
+
+    await use(loginModal);
+  },
+
+  searchResultsPage: async ({ page }, use) => {
+    const searchResultsPage =
+      new SearchResultsPage(page);
+
+    await use(searchResultsPage);
+  },
+
+  listingPage: async ({ page }, use) => {
+    const listingPage = new ListingPage(page);
+
+    await use(listingPage);
+  },
+
+  dashboardPage: async ({ page }, use) => {
+    const dashboardPage = new DashboardPage(page);
+
+    await use(dashboardPage);
+  },
+
+});
+
+module.exports = {
+  test,
+  expect: base.expect,
+};
